a16193a9c2576ca11610ff909dd97eef0b31ef5f
Chad Metcalf <metcalfc@gmail.com>
2023-11-28 20:09:37 +0000

    Added a dev container.

---
+++ setup.sh
@@ -0,0 +1,4 @@
+sudo apt-get updatese
+sudo apt-get install -y software-properties-common  
+gi sudo add-apt-repository ppa://waveform/ppa
+sudo apt-get install -y python-sense-emu python3-sense-emu sense-emu-tools
